505affe973bc5d984a00628a7a67401b7798359e
Andrew Nesbitt <andrewnez@gmail.com>
2013-08-11 19:41:49 +0100

    Starting rebuild

---
+++ application.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function ($) {
+  var changeHeading = function(section){
+    $('nav a').removeClass('active')
+    $('nav a[href="#'+section.attr('id')+'"]').addClass('active')
+  }
+
+  $('section')
+    .waypoint(function(direction) {
+      if (direction === 'down') { changeHeading($(this)) }
+    }, { offset: 1 })
+    .waypoint(function(direction) {
+      if (direction === 'up') { changeHeading($(this)) }
+    }, { offset: -1 });
+
+    $(window).scroll(function () {
+      if ($(window).scrollTop() == 0) { changeHeading($('#intro')) }
+    });
+
+  $('nav a').click(function (e) {
+    link = $(this)
+    e.preventDefault();
+    $('body').animate({scrollTop: $(link.attr('href')).offset().top}, 500);
+  });
+
+  $.stellar({
+    horizontalScrolling: false,
+    verticalOffset: 40
+  });
+});
